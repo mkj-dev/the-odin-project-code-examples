27ef35af807d6e3a98ab59b6b80bf6b0f31ed37f
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-07 21:01:45 +0000

    Add switch() version

--- javascript-basics/fizzbuzz/fizzbuzz.js
+++ javascript-basics/fizzbuzz/fizzbuzz.js
@@ -1,7 +1,7 @@
 // FizzBuzz game example
 let answer = parseInt(prompt('Enter any number for a Fizz Buzz game: '));
 
-for (let i = 1; i <= answer; i++) {
+/*for (let i = 1; i <= answer; i++) {
 	if (i % 3 === 0 && i % 5 ===0) {
 		console.log('FizzBuzz!');
 	} else if (i % 3 === 0) {
@@ -10,4 +10,21 @@ for (let i = 1; i <= answer; i++) {
 		console.log('Buzz');
 	} else {
 		console.log(i) }
+	} */
+
+for (let i = 1; i <= answer; i++) {
+	switch(true) {
+	case i % 3 === 0 && i % 5 === 0:
+		console.log('FizzBuzz');
+		break;
+	case(i % 3 === 0):
+		console.log('Fizz');
+		break;
+	case(i % 5 === 0):
+		console.log('Buzz');
+		break;
+	default:
+		console.log(i);
+		break;
 	}
+}
