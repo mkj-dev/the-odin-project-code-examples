32626dfc26828ceddedf12609de5169df9dc5b03
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-07-09 23:14:03 +0200

    Create blue h3

--- javascript-basics/dom-manipulation/script.js
+++ javascript-basics/dom-manipulation/script.js
@@ -1,9 +1,17 @@
 // container div
 const container = document.querySelector('.container');
 
-// creating first paragraph
+// create first paragraph
 const para = document.createElement('p');
 para.setAttribute('style', 'color:red;');
 para.textContent ='Hey! I\'m a red paragraph.';
 
 container.appendChild(para);
+
+// create blue h3
+const h3 = document.createElement('h3');
+h3.setAttribute('style', 'color: blue;');
+h3.textContent = 'I\'m blue.';
+
+container.appendChild(h3);
+
