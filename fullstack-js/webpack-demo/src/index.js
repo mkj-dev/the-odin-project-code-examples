d27f6b05f6d823683cd442801f63a0dfef400f50
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-08-10 21:28:19 +0200

    Import new elements from mathModule, add new elements to the webpage

--- fullstack-js/webpack-demo/src/index.js
+++ fullstack-js/webpack-demo/src/index.js
@@ -1,12 +1,24 @@
 import _ from 'lodash'; 
 import myName from './myName';
+import {number1, number2, addTwoNumbers, multiplyTwoNumbers} from './mathModule';
 
 function component() {
     const element = document.createElement('div');
+    const nameElement = document.createElement('p');
+    const additionElement = document.createElement('p');
+    const multiplicationElement = document.createElement('p');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerText = myName('Anonymous.');
+    // myName.js
+    nameElement.innerText = myName('Anonymous.');
+    // mathModule.js
+    additionElement.innerText = addTwoNumbers(number1, number2);
+    multiplicationElement.innerText = multiplyTwoNumbers(number1, number2);
+
+    element.appendChild(nameElement);
+    element.appendChild(additionElement);
+    element.appendChild(multiplicationElement);
 
     return element;
 }
