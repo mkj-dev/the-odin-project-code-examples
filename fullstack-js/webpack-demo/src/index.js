fdf08de019a6959d08496ed6e7b0a0432a4c11b0
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-08-12 21:59:57 +0200

    Cleanup for the output management tutorial

--- fullstack-js/webpack-demo/src/index.js
+++ fullstack-js/webpack-demo/src/index.js
@@ -1,39 +1,10 @@
 import _ from 'lodash'; 
-import myName from './myName';
-import {number1, number2, addTwoNumbers, multiplyTwoNumbers} from './mathModule';
-import './style.css';
-import Icon from './img/icon.png';
-import Data from './data/data.xml';
-import Notes from './data/data.csv';
 
 function component() {
     const element = document.createElement('div');
-    const nameElement = document.createElement('p');
-    const additionElement = document.createElement('p');
-    const multiplicationElement = document.createElement('p');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // myName.js
-    nameElement.innerText = myName('Anonymous.');
-    element.appendChild(nameElement);
-
-    // mathModule.js
-    additionElement.innerText = addTwoNumbers(number1, number2);
-    multiplicationElement.innerText = multiplyTwoNumbers(number1, number2);
-    element.appendChild(additionElement);
-    element.appendChild(multiplicationElement);
-
-    // Add icon to our existing div
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    // Console log data.csv and data.xml
-    console.log(Data);
-    console.log(Notes);
 
     return element;
 }
